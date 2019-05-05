e670ceb39ac20f897cc7eb8ecf973168b91c2cee
cplank <cplankshepard@gmail.com>
2019-05-04 19:18:59 -0700

    update works and it was really dumb

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -7,7 +7,8 @@ function Admin() {
         <div>
             HELLO!!!! Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 
-        <button className="button-test" type="button" onClick={() => Controller.findAll()}></button>
+        <button className="button-test btn-primary-lg" type="button" onClick={() => Controller.update()}>Update</button>
+            <button className="button-test btn-danger-lg" type="button" onClick={() => Controller.create()}>Create</button>
         </div>
     )
 }
--- src/utils/lifeAPIController.js
+++ src/utils/lifeAPIController.js
@@ -1,9 +1,7 @@
 //dependencies
-
 import axios from "axios";
 
-
-export function findAll() {
+export function create() {
     let data = {
         name: "Better Game Name",
         traits: {
@@ -42,6 +40,53 @@ export function findAll() {
     }
     axios
         .post("http://localhost:3001/api/games", data, config)
+        .then(results =>
+            results.data.items.filter(
+                result =>
+                    result.game.title
+            )
+        )
+}
+
+export function update() {
+    let data = {
+        name: "America's Ass",
+        traits: {
+            trait1: "Captain America",
+            trait2: "Cat",
+            trait3: "Little Monster",
+            trait4: "Adorbs",
+            trait5: "Meowzer"
+        },
+        Avatars: [{
+            name: "George",
+            trait1: 1,
+            trait2: 3,
+            trait3: 4,
+            trait4: 5,
+            trait5: 6
+        }],
+        Questions: [{
+            Q: "This is where a question would go, right?",
+            responses: [
+                { r1: "Giant", oc: [{ trait: "Hagrid", amount: 6, upDown: "NOMOREBOBS" }] },
+                { r2: "Bobs", oc: [{ trait: "Bobs", amount: 5, upDown: "Bobs" }] },
+                { r3: "Bobs", oc: [{ trait: "Bobs", amount: 4, upDown: "Bobs" }] },
+                { r4: "Bobs", oc: [{ trait: "Bobs", amount: 3, upDown: "Bobs" }] },
+                { r5: "Bobs", oc: [{ trait: "Bobs", amount: 2, upDown: "Bobs" }] }
+            ],
+            trait1: "Bobs",
+            trait2: "Bobs"
+        }]
+    }
+
+    let config = {
+        headers: {
+            "Access-Control-Allow-Origin": "anonymous"
+        }
+    }
+    axios
+        .put("http://localhost:3001/api/games/5ccddaa3bc49a849d88e6685", data, config)
         .then(results =>
             results.data.items.filter(
                 result =>
