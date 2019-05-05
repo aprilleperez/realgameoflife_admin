0b7f4e2556e46312bc6697e7cf9db7b919148c9a
cplank <cplankshepard@gmail.com>
2019-05-05 09:18:59 -0700

    added test data and made game class constructor

--- src/constructors.js
+++ src/constructors.js
@@ -11,17 +11,17 @@ class Traits {
 class Avatar {
     constructor(name, t1, t2, t3, t4, t5) {
         this.name = name
-        this.t1 = t1
-        this.t2 = t2
-        this.t3 = t3
-        this.t4 = t4
-        this.t5 = t5
+        this.trait1 = t1
+        this.trait2 = t2
+        this.trait3 = t3
+        this.trait4 = t4
+        this.trait5 = t5
     }
 }
 
 class Question {
-    constructor(q, responses, trait1, trait2) {
-        this.q = q
+    constructor(Q, responses, trait1, trait2) {
+        this.Q = Q
         this.responses = responses
         this.trait1 = trait1
         this.trait2 = trait2
@@ -29,9 +29,9 @@ class Question {
 }
 
 class Response {
-    constructor(r, oc) {
-        this.r = r
-        this.oc = oc
+    constructor(response, outcomes) {
+        this.response = response
+        this.outcomes = outcomes
     }
 }
 
@@ -42,3 +42,37 @@ class Outcome {
         this.upDown = upDown
     }
 }
+
+class GameObj {
+    constructor(name, traits, avatars, questions) {
+        this.name = name
+        this.traits = traits
+        this.avatars = avatars
+        this.questions = questions
+    }
+}
+
+let testTraits = new Traits("Red", "White", "Blue", "Firmness", "Bubble-ness")
+let testAvatars = [new Avatar("Steve Rogers", 20, 20, 20, 20, 20), new Avatar("Tony Stark", 10, 10, 10, 10, 10), new Avatar("Hulk", 18, 17, 16, 15, 14)]
+
+
+let testResponses = [
+    new Response("Definitely no favors done", [new Outcome("Firmness", 3, "down"), new Outcome("Red", 4, "up")]),
+    new Response("It was fine", [new Outcome("Bubble-ness", 4, "Up"), new Outcome("White", 3, "Down")]),
+    new Response("Typing all this blows", [new Outcome("Firmness", 5, "Down"), new Outcome("Firmness", 5, "down")])]
+
+
+
+let testQuestions = [new Question("Did Captain America's first suit really do him no favors?", testResponses, "Firmness", "Bubble-ness")]
+
+export const testGameObj = new GameObj("Still stuck on America's Ass", testTraits, testAvatars, testQuestions);
+
+let updateResponses = [
+    new Response("Because Tony is an appreciator of fine things", [new Outcome("Firmness", 3, "down"), new Outcome("Red", 4, "up")]),
+    new Response("He did it for his father", [new Outcome("Bubble-ness", 4, "Up"), new Outcome("White", 3, "Down")]),
+    new Response("Callipygian", [new Outcome("Firmness", 5, "Down"), new Outcome("Firmness", 5, "down")])]
+
+let updateQuestion = new Question("Why did Tony make Captain America's second suit so it did America's Ass Justice?", updateResponses, "Red", "Blue")
+
+export const updateGameObj = new GameObj(testGameObj.name, testGameObj.traits, testGameObj.avatars, [testQuestions[0], updateQuestion])
+
--- src/utils/lifeAPIController.js
+++ src/utils/lifeAPIController.js
@@ -1,45 +1,14 @@
 //dependencies
 import axios from "axios";
 
-export function create() {
-    let data = {
-        name: "America's Ass",
-        traits: {
-            trait1: "Red",
-            trait2: "White",
-            trait3: "Blue",
-            trait4: "Firmness",
-            trait5: "Bubble-ness"
-        },
-        Avatars: [{
-            name: "Steve Rogers",
-            trait1: 20,
-            trait2: 20,
-            trait3: 20,
-            trait4: 20,
-            trait5: 20
-        }],
-        Questions: [{
-            Q: "Did Captain America's first suit really do him no favors?",
-            responses: [
-                { r1: "Definitely no favors done", oc: [{ trait: "Firmness", amount: 6, upDown: "down" }] },
-                { r2: "It was fine", oc: [{ trait: "Bubble-ness", amount: 5, upDown: "up" }] },
-                { r3: "Neither is America's ass", oc: [{ trait: "Red", amount: 4, upDown: "down" }] },
-                { r4: "All the favors! 10/10", oc: [{ trait: "Blue", amount: 9, upDown: "up" }] },
-                { r5: "I'm neutral on Ameria's ass", oc: [{ trait: "White", amount: 2, upDown: "down" }] }
-            ],
-            trait1: "Bubble-ness",
-            trait2: "firmness"
-        }]
-    }
-
+export function create(gameObj) {
     let config = {
         headers: {
             "Access-Control-Allow-Origin": "anonymous"
         }
     }
     axios
-        .post("http://localhost:3001/api/games", data, config)
+        .post("http://localhost:3001/api/games", gameObj, config)
         .then(results =>
             results.data.items.filter(
                 result =>
@@ -48,37 +17,7 @@ export function create() {
         )
 }
 
-export function update() {
-    let data = {
-        name: "America's Ass",
-        traits: {
-            trait1: "Captain America",
-            trait2: "Cat",
-            trait3: "Little Monster",
-            trait4: "Adorbs",
-            trait5: "Meowzer"
-        },
-        Avatars: [{
-            name: "George",
-            trait1: 1,
-            trait2: 3,
-            trait3: 4,
-            trait4: 5,
-            trait5: 6
-        }],
-        Questions: [{
-            Q: "This is where a question would go, right?",
-            responses: [
-                { r1: "Giant", oc: [{ trait: "Hagrid", amount: 6, upDown: "NOMOREBOBS" }] },
-                { r2: "Bobs", oc: [{ trait: "Bobs", amount: 5, upDown: "Bobs" }] },
-                { r3: "Bobs", oc: [{ trait: "Bobs", amount: 4, upDown: "Bobs" }] },
-                { r4: "Bobs", oc: [{ trait: "Bobs", amount: 3, upDown: "Bobs" }] },
-                { r5: "Bobs", oc: [{ trait: "Bobs", amount: 2, upDown: "Bobs" }] }
-            ],
-            trait1: "Bobs",
-            trait2: "Bobs"
-        }]
-    }
+export function update(gameObj, id) {
 
     let config = {
         headers: {
@@ -86,7 +25,7 @@ export function update() {
         }
     }
     axios
-        .put("http://localhost:3001/api/games/5ccddaa3bc49a849d88e6685", data, config)
+        .put(`http://localhost:3001/api/games/${id}`, gameObj, config)
         .then(results =>
             results.data.items.filter(
                 result =>
