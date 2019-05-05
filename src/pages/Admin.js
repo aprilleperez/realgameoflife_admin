1cf5ac2f9724b5fda7fdf148de451e3877617fc0
cplank <cplankshepard@gmail.com>
2019-05-04 18:18:00 -0700

    got create working

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -1,10 +1,13 @@
 import React from "react";
+import * as Controller from "../utils/lifeAPIController";
 // import { Col, Row, Container } from "../components/Grid";
 
 function Admin() {
     return (
         <div>
             HELLO!!!! Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+        <button className="button-test" type="button" onClick={() => Controller.findAll()}></button>
         </div>
     )
 }
--- src/utils/lifeAPIController.js
+++ src/utils/lifeAPIController.js
@@ -1,16 +1,53 @@
 //dependencies
-const axios =require("axios");
 
-module.exports ={
-    findAll: function(req,res){
-        const { query: params } = req;
-        axios
-            .get("https://real-life-api.herokuapp.com/", { params })
-            .then(results => 
-                results.data.items.filter( 
-                    results =>
-                    result.game.title
-                )
-            )
+import axios from "axios";
+
+
+export function findAll() {
+    let data = {
+        name: "Better Game Name",
+        traits: {
+            trait1: "Bobs",
+            trait2: "Bobs",
+            trait3: "Bobs",
+            trait4: "Bobs",
+            trait5: "Bobs"
+        },
+        Avatars: [{
+            name: "Bobs",
+            trait1: 1,
+            trait2: 3,
+            trait3: 4,
+            trait4: 5,
+            trait5: 6
+        }],
+        Questions: [{
+            Q: "Bobs",
+            responses: [
+                { r1: "Bobs", oc: [{ trait: "Bobs", amount: 6, upDown: "Bobs" }] },
+                { r2: "Bobs", oc: [{ trait: "Bobs", amount: 5, upDown: "Bobs" }] },
+                { r3: "Bobs", oc: [{ trait: "Bobs", amount: 4, upDown: "Bobs" }] },
+                { r4: "Bobs", oc: [{ trait: "Bobs", amount: 3, upDown: "Bobs" }] },
+                { r5: "Bobs", oc: [{ trait: "Bobs", amount: 2, upDown: "Bobs" }] }
+            ],
+            trait1: "Bobs",
+            trait2: "Bobs"
+        }]
     }
-};
+
+    let config = {
+        headers: {
+            "Access-Control-Allow-Origin": "anonymous"
+        }
+    }
+    axios
+        .post("http://localhost:3001/api/games", data, config)
+        .then(results =>
+            results.data.items.filter(
+                result =>
+                    result.game.title
+            )
+        )
+}
+
+
