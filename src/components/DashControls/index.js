9463a86ef4b08b8b2396b73989401235efb04b5d
aprilleperez <aprillesakura@gmail.com>
2019-05-05 11:47:57 -0700

    added more components see index

---
+++ src/components/DashControls/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import '../style.css';
+
+export function DashControls() {
+    return (
+        <div className="thisControls text-center">
+            <i class="fas fa-play"></i>
+            <p> Play</ p>
+        </div>
+    )
+}
+
+
+export default DashControls;
