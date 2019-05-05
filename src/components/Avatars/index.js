eb53f2899916986e8d72a2f2ee282de8743a7fc0
cplank <cplankshepard@gmail.com>
2019-05-05 14:10:23 -0700

    started Avatar component

---
+++ src/components/Avatars/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Col, Row, Container } from '../components/Grid';
+import { Header, SubHeader } from '../components/Header'
+
+function Avatars() {
+    return (
+        <Container fluid>
+            <Header />
+            <SubHeader />
+            <Row>
+                <Col size="md-4">
+
+                </Col>
+
+            </Row>
+
+        </Container>
+    )
+}
+
+export default Avatars;
