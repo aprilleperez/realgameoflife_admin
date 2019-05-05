4aca4459ac97d5f67f57acd94cac0f66524becc7
cplank <cplankshepard@gmail.com>
2019-05-05 14:32:15 -0700

    added card component

---
+++ src/components/Card/index.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+function Card() {
+    return (
+        <div className="card mt-10">
+            <div className="img container">
+                <img src="./images/avatar.PNG"></img>
+            </div>
+            <div className="card-body">
+
+
+            </div>
+
+        </div>
+    )
+}
