fbcdc7ae5890a60f544c315e94c8c064fff4e222
aprilleperez <aprillesakura@gmail.com>
2019-05-08 09:58:31 -0700

    udpated admin page

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -58,8 +58,8 @@ class Admin extends Component {
                 <Header text={`Welcome to the dashboard, ${this.state.organization}.`} />
                 <SubHeader text="Create, Edit, or Drop games for your organization!" />
                 <Content games={this.state.allGameInfo} />
-                <AdminButton text="Create" buttonType="green" to="/create" />
-                <AdminButton text="Edit" buttonType="blue" to="/edit" />
+                <AdminButton text="Create Game" buttonType="green" to="/create" />
+                {/* <AdminButton text="Edit" buttonType="blue" to="/edit" /> */}
 
                 <button className="btn btn-primary" type="button" onClick={() => Controller.update(updateGameObj, "5ccf266b84b9a54c70ed7ba9")}>Update</button>
                 <button className="btn btn-danger" type="button" onClick={() => Controller.create(testGameObj)}>Create</button>
