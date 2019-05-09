8d8b3b3bc70852aec52885b9ee4950a5df759ade
aprilleperez <aprillesakura@gmail.com>
2019-05-09 11:35:19 -0700

    avatar selection shows up but still need to link correctly

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 import { Container } from '../components/Grid';
-import { Header, SubHeader } from '../components/Header'
+import { Header, SubHeader, AvatarHeader } from '../components/Header'
 import { QuestionDropdown } from '../components/Dropdown'
 import Content from '../components/Content'
 import AdminButton from '../components/Button'
@@ -27,7 +27,8 @@ class Create extends Component {
                 {window.location.pathname === "/create/avatars" ? (
                     <Container fluid>
                         <Header text="Create Avatars" />
-                        <SubHeader text="Avatar Tabs will go here" />
+                        {/* <SubHeader text="Avatar Tabs will go here" /> */}
+                        <AvatarHeader />
                         <Content />
                         <AdminButton text="Next" buttonType="green" to="/create/questions" />
                     </Container>
