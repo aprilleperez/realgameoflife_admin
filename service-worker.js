711e0444a573491a1e3d9d3f19c0c1cf2d112cbc
aprilleperez <aprillesakura@gmail.com>
2019-05-09 15:07:22 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/realgamesoflife_admin/precache-manifest.e931c7a20ae2adb00e57e9428728de9c.js"
+  "/realgameoflife_admin/precache-manifest.fe9d4786073469d447be018c59c7fcf4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/realgamesoflife_admin/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/realgameoflife_admin/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
