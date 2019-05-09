0634f68cc92af672fb96f8cb14f578b54298be95
aprilleperez <aprillesakura@gmail.com>
2019-05-09 14:55:16 -0700

    Updates

---
+++ precache-manifest.e931c7a20ae2adb00e57e9428728de9c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c72460c5389787dc1c53fea6699a156",
+    "url": "/realgamesoflife_admin/index.html"
+  },
+  {
+    "revision": "d39e6aa374022314ac50",
+    "url": "/realgamesoflife_admin/static/css/main.a16ca5bc.chunk.css"
+  },
+  {
+    "revision": "34f8e9b12b6e07eee3c9",
+    "url": "/realgamesoflife_admin/static/js/2.35a6896d.chunk.js"
+  },
+  {
+    "revision": "d39e6aa374022314ac50",
+    "url": "/realgamesoflife_admin/static/js/main.8775a89e.chunk.js"
+  },
+  {
+    "revision": "3c10e990604d0d0ff2fe",
+    "url": "/realgamesoflife_admin/static/js/runtime~main.5f4442a9.js"
+  }
+]);
