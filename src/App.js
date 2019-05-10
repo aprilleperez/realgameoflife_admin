582f9a62425f481073834cbb5133fd572a95773e
aprilleperez <aprillesakura@gmail.com>
2019-05-10 10:27:06 -0700

    stuff

--- src/App.js
+++ src/App.js
@@ -54,9 +54,9 @@ class App extends Component {
         <div>
           <Nav />
           <Switch>
-            <Route exact path= {`${process.env.PUBLIC_URL}/`} render={(props) => <Admin {...props} globalState={this.state} />} />
-            <Route prefix path={`${process.env.PUBLIC_URL}/create`}render={(props) => <Create {...props} globalState={this.state} />} />
-            <Route prefix path={`${process.env.PUBLIC_URL}/edit`} render={(props) => <Edit {...props} globalState={this.state} />} />
+            <Route exact path= {`/`} render={(props) => <Admin {...props} globalState={this.state} />} />
+            <Route prefix path={`/create`}render={(props) => <Create {...props} globalState={this.state} />} />
+            <Route prefix path={`/edit`} render={(props) => <Edit {...props} globalState={this.state} />} />
             {/* <Route component={NoMatch} /> */}
           </Switch>
         </div>
