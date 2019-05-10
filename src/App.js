4bd5fc7ee7e50f7038ee40e0e5204cf442c5daa6
aprilleperez <aprillesakura@gmail.com>
2019-05-10 10:24:23 -0700

    pls

--- src/App.js
+++ src/App.js
@@ -50,7 +50,7 @@ class App extends Component {
   }
   render() {
     return (
-      <Router>
+      <Router basename = {process.env.PUBLIC_URL}>
         <div>
           <Nav />
           <Switch>
--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -21,7 +21,7 @@ class Admin extends Component {
                 <Header text={`Welcome to the dashboard, ${this.props.globalState.organization}.`} />
                 <SubHeader text="Create, Edit, or Drop games for your organization!" />
                 <Content gameNameGameId={this.props.globalState.gameNameGameId} everyGame={this.props.globalState.allGames} />
-                <AdminButton text="Create Game" buttonType="green" to="/create" />
+                <AdminButton text="Create Game" buttonType="green" to={`${process.env.PUBLIC_URL}/create`} />
                 {/* <AdminButton text="Edit" buttonType="blue" to="/edit" /> */}
 
                 <button className="btn btn-primary" type="button" onClick={() => Controller.update(updateGameObj, "5ccf266b84b9a54c70ed7ba9")}>Update</button>
