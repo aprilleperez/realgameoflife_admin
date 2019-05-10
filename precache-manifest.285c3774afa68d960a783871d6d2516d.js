7219e7dd871888610df38666ef9310a9548c618c
aprilleperez <aprillesakura@gmail.com>
2019-05-10 10:05:24 -0700

    Updates

--- precache-manifest.285c3774afa68d960a783871d6d2516d.js
+++ precache-manifest.285c3774afa68d960a783871d6d2516d.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "12e604216c1d082f98794a8c0490cd25",
+    "revision": "75df8f2e9b2029ff831de44c40a3c17a",
     "url": "/realgameoflife_admin/index.html"
   },
   {
-    "revision": "01c450dba697dcbbd4e1",
+    "revision": "499b7452a9314ae2995f",
     "url": "/realgameoflife_admin/static/css/main.a16ca5bc.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/realgameoflife_admin/static/js/2.6c553b60.chunk.js"
   },
   {
-    "revision": "01c450dba697dcbbd4e1",
-    "url": "/realgameoflife_admin/static/js/main.cccd6bae.chunk.js"
+    "revision": "499b7452a9314ae2995f",
+    "url": "/realgameoflife_admin/static/js/main.05f6ea14.chunk.js"
   },
   {
     "revision": "ff9e2e72e1b80b41c79f",
